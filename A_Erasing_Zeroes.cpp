2905f7fa6bd48133baae77ab5a781e218045cc16
Shubham Bharti <shubhambharti409sb5019175@gmail.com>
2025-12-29 12:31:37 +0530

    Add new files and update existing ones

--- A_Erasing_Zeroes.cpp
+++ A_Erasing_Zeroes.cpp
@@ -1,15 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define fastio ios::sync_with_stdio(false); cin.tie(NULL);
+#define fastio                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);
 
-int main() {
+int main()
+{
     fastio
 
-    int t;
+        int t;
     cin >> t;
-    while(t--) {
-        
+    while (t--)
+    {
+        string str;
+        cin>>str;
+        int firstIdx = 0;
+        int lastIdx = 0;
+        for (int i = 0; i < str.length(); i++)
+        {
+            if (str[i] == '1')
+            {
+                firstIdx = i;
+                break;
+            }
+        }
+        for (int i = str.length() - 1; i >= 0; i--)
+        {
+            if (str[i] == '1')
+            {
+                lastIdx = i;
+                break;
+            }
+        }
+        int count = 0;
+
+        for (int i = firstIdx + 1; i < lastIdx; i++)
+        {
+            if (str[i] == '0')
+            {
+                count++;
+            }
+        }
+
+        cout << count << endl;
     }
 
     return 0;
